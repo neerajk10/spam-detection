5e7d629b8143f3fa0cc88f90c54ef6f400f0a087
sweetpoision <42069468+NeerajKondalkar@users.noreply.github.com>
2021-04-08 22:01:54 +0530

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -12,8 +12,8 @@ myapp = Flask(__name__)
 
 @myapp.route('/predict', methods=['POST'])
 def predict():
-    # model = pickle.load(open("../model/spam_model.pkl", "rb"))
-    # tfidf_model = pickle.load(open("../model/tfidf_model.pkl", "rb"))
+    model = pickle.load(open("../model/spam_model.pkl", "rb"))
+    tfidf_model = pickle.load(open("../model/tfidf_model.pkl", "rb"))
     if request.method == "POST":
         bool_is_json = request.is_json
         if not bool_is_json:
@@ -26,12 +26,38 @@ def predict():
             print("id : ", id)           
             print("number : ", number)
             print("message: ", message)
-            spam = False
+            spam = False #set by DMM 
+
+            message = message.str.replace(
+            r'^.+@[^\.].*\.[a-z]{2,}$', 'emailaddress')
+            message = message.str.replace(
+                r'^http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$', 'webaddress')
+            message = message.str.replace(r'£|\$', 'money-symbol')
+            message = message.str.replace(
+                r'^\(?[\d]{3}\)?[\s-]?[\d]{3}[\s-]?[\d]{4}$', 'phone-number')
+            message = message.str.replace(r'\d+(\.\d+)?', 'number')
+            message = message.str.replace(r'[^\w\d\s]', ' ')
+            message = message.str.replace(r'\s+', ' ')
+            message = message.str.replace(r'^\s+|\s*?$', ' ')
+            message = message.str.lower()
+
+            stop_words = set(stopwords.words('english'))
+            message = message.apply(lambda x: ' '.join(
+                term for term in x.split() if term not in stop_words))
+            ss = nltk.SnowballStemmer("english")
+            message = message.apply(lambda x: ' '.join(ss.stem(term)
+                                                                    for term in x.split()))
+
+            # tfidf_model = TfidfVectorizer()
+            tfidf_vec = tfidf_model.transform(message)
+            tfidf_data = pd.DataFrame(tfidf_vec.toarray())
+            my_prediction = model.predict(tfidf_data)
+
+            spam = my_prediction
+
             # str = {"result" : "This be a valid req niqqa", "id": "" + id + ""}
             result = {"result" : "This be a valid req niqqa", "id": "" + id + "", "spam" : "" + str(spam) + ""}
             return json.dumps(result)
-            # return json.jsonify("{\"result\" : \"This be a valid req niqqa\", \"id\": \"" + id + "\"}"), 200
-            # return json.jsonify("{'result' : 'This be a valid req niqqa', 'id': '" + id + "'}"), 200
     else:
             return json.jsonify("{\"result\": \"API only responds to POST request\"}"), 400
 
